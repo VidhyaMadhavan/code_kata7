062630706953b33dbeee41c0149e45b1c8878d69
VidhyaMadhavan <vidhya.m1818@gmail.com>
2016-10-21 11:20:59 -0700

    Update natural.java

--- natural.java
+++ natural.java
@@ -8,11 +8,13 @@ Scanner sc=new Scanner(System.in);
 int n,sum=0,i=1;
 System.out.println("Enter total natural number for finding sum:");
 n=sc.nextInt();
+  if(n<=0)
+System.out.println("Error");
 while(i<=n)
 {
 sum=sum+i;
 i++;
-}
 System.out.println("Sum of natural number is:"+sum);
+}
 }
 }
